7b9aae2cbe5881feca260fd07a65d59875fe205a
wowdigis <shubham@wowdigitalcentre.com>
2024-08-21 23:06:50 +0530

    changes added

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -54,7 +54,7 @@ const experience = {
       duration: "August 2023 - Present",
     },
     {
-      company: "Mobiloitte Technologies Pvt Ltd. Pune",
+      company: "Mobiloitte Technologies Pvt Ltd.",
       position: "Full Stack Developer",
       duration: "Dec 2022 - July 2023",
     },
@@ -155,14 +155,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 import { motion } from "framer-motion";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 const Resume = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{
-        opacity:1,
-        transition:{ delay: 2.4, duration: 0.4, ease:"easeIn" }
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
       }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
@@ -178,21 +179,59 @@ const Resume = () => {
             <TabsTrigger value="about">About Me</TabsTrigger>
           </TabsList>
 
-      <div className="min-h-[70vh] w-full" >
-        <TabsContent value="experience" className="w-full">
-          Experience
-        </TabsContent>
-        <TabsContent value="education" className="w-full">
-        Education
-        </TabsContent>
-        <TabsContent value="skills" className="w-full">
-        Skills
-        </TabsContent>
-        <TabsContent value="about" className="w-full">
-        About Me
-        </TabsContent>
+          <div className="min-h-[70vh] w-full" >
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index)=> {
+                      return (
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                {/* <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p> */}
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index)=> {
+                      return (
+                        <li key={index} className="bg-[#232329] h-[250px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[100px] text-center lg:text-left">{item.institution}</h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.degree}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              Skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              About Me
+            </TabsContent>
 
-      </div>
+          </div>
 
 
         </Tabs>
